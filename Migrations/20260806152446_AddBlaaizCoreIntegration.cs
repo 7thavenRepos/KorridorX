a4e93f37470a4d57cf5c0ceedff9949334fc5dcc
Olukayode <olukayode.adebiyi@hotmail.co.uk>
2026-08-06 10:44:56 -0500

    Add ProviderExpiresAt column and unique index updates
    Added nullable ProviderExpiresAt column to Collections table.
    Created unique index on ProviderCode and CustomerProfileId in ProviderCustomers.
    Updated EF Core migration files and AppDbContextModelSnapshot accordingly.

---
+++ Migrations/20260806152446_AddBlaaizCoreIntegration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KorridorX.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBlaaizCoreIntegration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ProviderExpiresAt",
+                table: "Collections",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProviderCustomers_ProviderCode_CustomerProfileId",
+                table: "ProviderCustomers",
+                columns: new[] { "ProviderCode", "CustomerProfileId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ProviderCustomers_ProviderCode_CustomerProfileId",
+                table: "ProviderCustomers");
+
+            migrationBuilder.DropColumn(
+                name: "ProviderExpiresAt",
+                table: "Collections");
+        }
+    }
+}
